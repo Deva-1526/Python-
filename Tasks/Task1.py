a72c2a01f3848cb5ef6e9a1f0c68c2c947fd0418
Deva-1526 <rdevadharshini555@gmail.com>
2026-02-19 20:32:50 +0530

    added

--- Tasks/Task1.py
+++ Tasks/Task1.py
@@ -2,4 +2,13 @@
 a="Hello World !"
 print(a)
 
-#Add,Sub,Multiply,Divide,Modules of Two Numbers
+#Add,Sub,Multiply,Divide,Modules of Two Numbers.
+a=10
+b=20
+print("The addition of a & b:", a+b)
+print("The subtraction of a&b:", a-b)
+print("The multiplication of a&b:", a*b)
+print("The division of a&b:", a/b)
+print("The modulus of a&b:", a%b)
+
+#Square Root 
