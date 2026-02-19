a3b8531af5303d7baf326049887741269d8d7a15
Deva-1526 <rdevadharshini555@gmail.com>
2026-02-19 20:36:15 +0530

    added

--- Tasks/Task1.py
+++ Tasks/Task1.py
@@ -21,3 +21,10 @@ base=30
 height=50
 area=0.5*base*height
 print("Area of triangle:", area)
+
+#Quadratic function
+a=60
+b=90
+print("(a+b)^2=", a+b**2)
+print("(a-b)^2=", a-b**2)
+print("a^2-b^2=", (a**2)-(b**2))
