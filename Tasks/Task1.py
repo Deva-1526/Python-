7476f91a07fa0646ed94bb60d4b917f2781d6455
Deva-1526 <rdevadharshini555@gmail.com>
2026-02-19 20:39:32 +0530

    Add functionality to print last digit and last two digits

--- Tasks/Task1.py
+++ Tasks/Task1.py
@@ -48,3 +48,12 @@ print("without using variable")
 print("swapping:")
 print("a=", a)
 print("b=",b)
+
+#last digit of the number
+num=38950
+last_digit = num%10
+print("Last digit:", last_digit)
+#last two digit of the number
+num=2346528
+last_two_digit= num%100
+print("Last two digit:",last_two_digit)
